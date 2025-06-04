ed9fb1412e1e69a2eaf6a08ef3369bc07f3fa8a1
Sawangmo <07230042.sherubtse@rub.edu.bt>
2025-06-04 15:47:05 +0600

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
-const { ensureAdmin } = require('../middleware/authMiddleware');
+const { ensureAdmin } = require('../Middleware/authMiddleware');
 
 // Protect all admin routes
 router.use(ensureAdmin);
